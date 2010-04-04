5cf8d0907752871d4ce33b19ab5c5d09aee12a14
Max Lapan <max.lapan@gmail.com>
2010-04-04 15:38:24 +0400

    Still WA

--- contests/2010.April/TRIP/trip.c
+++ contests/2010.April/TRIP/trip.c
@@ -3,86 +3,37 @@
 
 
 int n, vol;
-int dists[1000000];
-
-
-void solve (int pos, int rest, int* stops, int* vars)
-{
-    int a, b, d;
-    int v1, v2;
-
-    //    printf ("solve: %d, %d\n", pos, rest);
-
-    if (pos == n-1) {
-        *stops = 0;
-        *vars = 1;
-        return;
-    }
-    d = dists[pos+1];
-
-    if (d <= rest) {
-        solve (pos+1, rest - d, &a, &v1);
-        solve (pos+1, vol - d, &b, &v2);
-        b++;
-
-        if (a > b) {
-            v1 = v2;
-            a = b;
-        }
-        else {
-            if (a == b)
-                v1++;
-        }
-            
-    }
-    else {
-        solve (pos+1, vol - d, &a, &v1);
-        a++;
-    }
-
-    *stops = a;
-    *vars = v1;
-}
-
-
-void solve2 (int* stops, int* vars)
-{
-    int s, v, i, d;
-
-    s = 0;
-    v = vol;
-    *vars = 0;
-
-    for (i = 1; i < n; i++) {
-        d = dists[i];
-        if (v < d) {
-            s++;
-            v = vol;
-        }
-        else
-            (*vars)++;
-        v -= d;
-    }
-
-    *stops = s;
-}
 
 
 int main(int argc, char *argv[])
 {
-    int i, stops, vars, prev = 0, val;
+    int i, stops, vars, prev = 0, val, c_vol, d;
 
     scanf ("%d %d", &n, &vol);
+
+    stops = 0;
+    vars = 0;
+    c_vol = vol;
+
+    scanf ("%d", &prev);
     
-    for (i = 0; i < n; i++) {
+    for (i = 1; i < n; i++) {
         scanf ("%d", &val);
-        dists[i] = val-prev;
+        d = val-prev;
         prev = val;
+
+        if (c_vol < d) {
+            stops++;
+            c_vol = vol;
+        }
+        else {
+            vars++;
+            vars %= 1000000007;
+        }
+        c_vol -= d;
     }
 
-    //    solve (0, vol, &stops, &vars);
-    solve2 (&stops, &vars);
-    printf ("%d %d\n", stops, vars % 1000000007);
+    printf ("%d %d\n", stops, vars);
 
     return 0;
 }

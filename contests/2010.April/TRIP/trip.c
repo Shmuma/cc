720e0e3f9c4c3e592f7dd7292ed600ac3c9b0b9b
Max Lapan <max.lapan@gmail.com>
2010-04-04 14:09:41 +0400

    WA

--- contests/2010.April/TRIP/trip.c
+++ contests/2010.April/TRIP/trip.c
@@ -5,28 +5,25 @@
 int n, vol;
 int dists[1000000];
 
-int cache[1000000][2];
 
-
-void solve (int pos, int rest)
+void solve (int pos, int rest, int* stops, int* vars)
 {
     int a, b, d;
     int v1, v2;
 
     //    printf ("solve: %d, %d\n", pos, rest);
 
-    if (pos == n-1)
+    if (pos == n-1) {
+        *stops = 0;
+        *vars = 1;
         return;
-    d = dists[pos+1] - dists[pos];
+    }
+    d = dists[pos+1];
 
     if (d <= rest) {
-        solve (pos+1, rest - d);
-        a = cache[pos+1][0];
-        v1 = cache[pos+1][1];
-
-        solve (pos+1, vol - d);
-        b = cache[pos+1][0]+1;
-        v2 = cache[pos+1][1];
+        solve (pos+1, rest - d, &a, &v1);
+        solve (pos+1, vol - d, &b, &v2);
+        b++;
 
         if (a > b) {
             v1 = v2;
@@ -39,29 +36,53 @@ void solve (int pos, int rest)
             
     }
     else {
-        solve (pos+1, vol - d);
-        a = cache[pos+1][0]+1;
-        v1 = cache[pos+1][1];
+        solve (pos+1, vol - d, &a, &v1);
+        a++;
     }
 
-    cache[pos][0] = a;
-    cache[pos][1] = v1;
+    *stops = a;
+    *vars = v1;
+}
+
+
+void solve2 (int* stops, int* vars)
+{
+    int s, v, i, d;
+
+    s = 0;
+    v = vol;
+    *vars = 0;
+
+    for (i = 1; i < n; i++) {
+        d = dists[i];
+        if (v < d) {
+            s++;
+            v = vol;
+        }
+        else
+            (*vars)++;
+        v -= d;
+    }
+
+    *stops = s;
 }
 
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int i, stops, vars, prev = 0, val;
 
     scanf ("%d %d", &n, &vol);
     
-    for (i = 0; i < n; i++)
-        scanf ("%d", dists+i);
+    for (i = 0; i < n; i++) {
+        scanf ("%d", &val);
+        dists[i] = val-prev;
+        prev = val;
+    }
 
-    cache[n-1][0] = 0;
-    cache[n-1][1] = 1;
-    solve (0, vol);
-    printf ("%d %d\n", cache[0][0], cache[0][1]);
+    //    solve (0, vol, &stops, &vars);
+    solve2 (&stops, &vars);
+    printf ("%d %d\n", stops, vars % 1000000007);
 
     return 0;
 }

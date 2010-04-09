473e237247aea4abed3e27b11885db56895cf577
Max Lapan <max.lapan@gmail.com>
2010-04-09 16:25:21 +0400

    New method (via moving free sticks). Still WA :(

--- contests/2010.April/TRIP/trip.c
+++ contests/2010.April/TRIP/trip.c
@@ -4,56 +4,57 @@
 
 int n, vol;
 int deltas[1000001];
+int rest[1000001];
+int feed[1000001];
 
 
 int main(int argc, char *argv[])
 {
     int i, stops, vars, prev = 0, val, c_vol, j;
+    int prev_feed;
 
     scanf ("%d %d", &n, &vol);
 
-    stops = 0;
-    vars = 1;
-    c_vol = vol;
-
     scanf ("%d", &prev);
-    
     for (i = 1; i < n; i++) {
         scanf ("%d", &val);
         deltas[i-1] = val-prev;
         prev = val;
+    }
+
+    prev_feed = 0;
+    stops = 0;
+    c_vol = vol;
+
+    for (i = 1; i < n; i++) {
+        rest[i-1] = c_vol;
 
         if (c_vol < deltas[i-1]) {
             stops++;
             stops %= 1000000007;
-            if (c_vol > 0) {
-                int d = c_vol;
-
-                /* step back to find amount of ways to plan */
-                for (j = i-1; j >= 0; j++) {
-                    if (c_vol > 0) {
-                        c_vol -= deltas[j];
-                        vars++;
-                        vars %= 1000000007;
-                        break;
-                    }
-                }
-            }
             c_vol = vol;
+            prev_feed = i-1;
         }
         c_vol -= deltas[i-1];
+        feed[i-1] = prev_feed;
     }
 
-    if (c_vol > 0) {
-        int d = c_vol;
+    rest[n-1] = c_vol;
 
-        /* step back to find amount of ways to plan */
-        for (j = n-2; j >= 0; j++) {
-            if (c_vol > 0) {
-                c_vol -= deltas[j];
-                vars++;
-                vars %= 1000000007;
-                break;
+    /* count amount of variables */
+    vars = 1;
+    for (i = n-1; i >= 0; i--) {
+        if (rest[i] > 0 && (feed[i] == i || i == n-1)) {
+            c_vol = rest[i];
+            /* if we filled our car at this point AND we had fuel, we can move previous filling point c_vol left */
+            for (j = feed[i-1]; j > 0; j--) {
+                if (c_vol >= deltas[j-1]) {
+                    vars++;
+                    vars %= 1000000007;
+                    c_vol -= deltas[j-1];
+                }
+                else
+                    break;
             }
         }
     }

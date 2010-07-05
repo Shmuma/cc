6b2537f04de37cc521f4c068bad4e56f0325081b
Max Lapan <max.lapan@gmail.com>
2010-07-05 23:54:14 +0400

    MAXCROSS accepted

--- contests/2010.July/MAXCROSS/cross.c
+++ contests/2010.July/MAXCROSS/cross.c
@@ -4,41 +4,16 @@
 
 int n;
 
-char dat[1002][1002];
+char dat[1001][1001];
 
-unsigned int buf[8][1002][1002];
-
-void scan (int index,
-           int min_i, int max_i, int step_i,
-           int min_j, int max_j, int step_j,
-           int delta_i, int delta_j)
-{
-    int i, j;
-
-    i = min_i;
-
-    while (i != max_i+step_i) {
-        j = min_j;
-
-        while (j != max_j+step_j) {
-            if (!dat[i][j])
-                buf[index][i][j] = 0;
-            else
-                buf[index][i][j] = 1 + buf[index][i + delta_i][j + delta_j];
-
-            j += step_j;
-        }
-
-        i += step_i;
-    }
-}
+unsigned int buf[4][1001][1001];
 
 
 int get_max (int i, int j)
 {
     int t, res = buf[0][i][j];
 
-    for (t = 1; t < 8; t++)
+    for (t = 1; t < 4; t++)
         if (res < buf[t][i][j])
             res = buf[t][i][j];
 
@@ -46,31 +21,88 @@ int get_max (int i, int j)
 }
 
 
+void scan (int index, int i, int j, int di, int dj)
+{
+    int x = i, y = j, t = 0;
+
+    while (x < n && y < n && dat[x][y]) {
+        x += di;
+        y += dj;
+        t++;
+    }
+
+    x = i;
+    y = j;
+    
+    while (x < n && y < n && dat[x][y]) {
+        buf[index][x][y] = t;
+        x += di;
+        y += dj;
+    }
+}
+
+
+void scan_neg (int index, int i, int j, int di, int dj)
+{
+    int x = i, y = j, t = 0;
+
+    while (x >= 0 && y < n && dat[x][y]) {
+        x += di;
+        y += dj;
+        t++;
+    }
+
+    x = i;
+    y = j;
+    
+    while (x >= 0 && y < n && dat[x][y]) {
+        buf[index][x][y] = t;
+        x += di;
+        y += dj;
+    }
+}
+
+
+
 int main (int argc, char *argv[])
 {
-    int i, j;
+    int i, j, k;
+    static char tmp[2000];
+    int deltas[3][2] = {
+        {0, 1},
+        {1, 0},
+        {1, 1}
+    };
 
     scanf ("%d", &n);
     getchar ();
 
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++)
-            dat[i][j] = getchar () == 'X' ? 1 : 0;
+    for (i = 0; i < n; i++) {
+        scanf ("%s", tmp);
+        for (j = 0; j < n; j++)
+            dat[i][j] = tmp[j] == 'X' ? 1 : 0;
         getchar ();
     }
 
     memset (buf, 0, sizeof (buf));
 
-    scan (0, 1, n,  1, 1, n,  1, -1,  0);
-    scan (1, n, 1, -1, 1, n,  1,  1,  0);
-    scan (2, 1, n,  1, 1, n,  1,  0, -1);
-    scan (3, 1, n,  1, n, 1, -1,  0,  1);
+    /* three pases are positive */
+    for (k = 0; k < 3; k++) {
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                if (dat[i][j] && !buf[k][i][j])
+                    scan (k, i, j, deltas[k][0], deltas[k][1]);
+    }
+    /* and one is negative */
+    for (i = n-1; i >= 0; i--)
+        for (j = 0; j < n; j++)
+            if (dat[i][j] && !buf[3][i][j])
+                scan_neg (3, i, j, -1, 1);
 
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
-            if (j > 1)
-                putchar (' ');
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             printf ("%d", get_max (i, j));
+            putchar (' ');
         }
         putchar ('\n');
     }

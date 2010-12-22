5aa6b7f7caf6ccf148377f0122c8fbb8034151e7
Max Lapan <max.lapan@gmail.com>
2010-12-22 11:58:10 +0300

    Palindrome generation

--- easy/PrPalin/prpalin.c
+++ easy/PrPalin/prpalin.c
@@ -6,7 +6,7 @@ unsigned int digits[50];
 unsigned int digits_count;
 
 
-void num_to_digits (int n)
+void num_to_digits (unsigned long long n)
 {
     digits_count = 0;
     while (n > 0) {
@@ -16,40 +16,102 @@ void num_to_digits (int n)
 }
 
 
-int next_palindrome ()
+unsigned long long digits_to_num (unsigned int* arr, unsigned int count)
 {
-    int i, n;
-    int flag = 0
+    int i;
+    unsigned long long res = 0;
 
-    for (i = 0; ; i++) {
-        n = digits_count - i - 1;
-        if (i >= n)
-            break;
-        if (digits[i] > digits[n]) {
-            digits[n] = digits[i];
-            flag = 1;
-            break;
-        }
-        if (digits[i] < digits[n])
+    for (i = count-1; i >= 0; i--) {
+        res *= 10;
+        res += arr[i];
     }
+
+    return res;
 }
 
 
-unsigned long long solve (int n)
-{
-    num_to_digits (n);
-    next_palindrome ();
 
-    return n;
+unsigned long long digits_to_num_inv (unsigned int* arr, unsigned int count)
+{
+    int i;
+    unsigned long long res = 0;
+
+    for (i = 0; i < count; i++) {
+        res *= 10;
+        res += arr[i];
+    }
+
+    return res;
+}
+
+
+
+void dissect (unsigned long long n, unsigned long long* base, int* extra, unsigned long long* rest_inv, unsigned long long* base_inv)
+{
+    unsigned int base_count;
+
+    num_to_digits (n);
+    base_count = digits_count >> 1;
+
+    if (digits_count % 2) {
+        *base = digits_to_num (digits+(base_count+1), base_count);
+        *base_inv = digits_to_num_inv (digits+(base_count+1), base_count);
+        *extra = digits[base_count];
+    }
+    else {
+        *base = digits_to_num (digits + base_count, base_count);
+        *base_inv = digits_to_num_inv (digits + base_count, base_count);
+        *extra = -1;
+    }
+    *rest_inv = digits_to_num_inv (digits, base_count);
+}
+
+
+
+unsigned long long make_palin (unsigned long long base, int extra)
+{
+    unsigned long long res = base;
+    int i;
+
+    if (extra >= 0) {
+        res *= 10;
+        res += extra;
+    }
+
+    num_to_digits (base);
+
+    for (i = 0; i < digits_count; i++)
+        res *= 10;
+    return res + digits_to_num_inv (digits, digits_count);
+}
+
+
+
+unsigned long long solve (unsigned long long base, int extra)
+{
+    int i;
+
+    for (i = 0; i < 100; i++)
+        printf ("%llu\n", make_palin (base+i, extra));
+
+    return make_palin (base, extra);
 }
 
 
 int main (int argc, char *argv[])
 {
-    int n;
+    unsigned long long n, base, base_inv, rest_inv, palin;
+    int extra;
 
-    scanf ("%d", &n);
+    scanf ("%llu", &n);
 
-    printf ("%u\n", solve (n));
+    dissect (n, &base, &extra, &rest_inv, &base_inv);
+
+    if (base_inv > rest_inv)
+        palin = solve (base, extra);
+    else
+        palin = solve (base+1, extra);
+
+    printf ("%llu\n", palin);
     return 0;
 }

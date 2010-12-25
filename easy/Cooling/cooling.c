0ffe996285c581c9ee593391116031b2de682992
Max Lapan <max.lapan@gmail.com>
2010-12-25 11:23:03 +0300

    Need another nice idea

--- easy/Cooling/cooling.c
+++ easy/Cooling/cooling.c
@@ -5,6 +5,22 @@ int weight[40];
 int limit[40];
 int max;
 
+int places[40][40];
+int len[40];
+int fact_len[40];
+
+
+void clear ()
+{
+    memset (weight, 0, sizeof (weight));
+    memset (limit, 0, sizeof (limit));
+
+    memset (places, 0, sizeof (places));
+    memset (len, 0, sizeof (len));
+    memset (fact_len, 0, sizeof (len));
+    max = 0;
+}
+
 
 int comp (const void* a, const void* b)
 {
@@ -15,47 +31,69 @@ int comp (const void* a, const void* b)
 }
 
 
-int solve (int w_ofs, int l_ofs)
+int solve ()
 {
-    int res = 0, val;
-    if (w_ofs == max || l_ofs == max)
-        return 0;
+    int i, j, min_idx, res = 0;
+    int min;
 
-    if (weight[w_ofs] <= limit[l_ofs])
-        res = 1+solve (w_ofs+1, l_ofs+1);
+    /* build places table */
+    for (i = 0; i < max; i++)
+        for (j = max-1; j >= 0; j--) {
+            if (weight[i] > limit[j])
+                break;
+            places[i][len[i]++] = j;
+            fact_len[i]++;
+        }
 
-    val = solve (w_ofs, l_ofs+1);
-    if (val > res)
-        res = val;
-    /* val = solve (w_ofs+1, l_ofs); */
-    /* if (val > res) */
-    /*     res = val; */
-    /* val = solve (w_ofs+1, l_ofs+1); */
-    /* if (val > res) */
-    /*     res = val; */
+    for (i = 0; i < max; i++) {
+        min = 100;
+        min_idx = -1;
+
+        /* find pie with min amount of available places */
+        for (j = 0; j < max; j++)
+            if (min > fact_len[j] && fact_len[j] > 0) {
+                min = fact_len[j];
+                min_idx = j;
+            }
+
+        if (min_idx == -1)
+            break;
+
+        /* for each available place check for amount of pies we will throw out if we'll take this place */
+        for (j = 0; j < len[min_idx]; j++) {
+            if (places[min_idx][j] < 0)
+                continue;
+        }
+    }
+
     return res;
 }
 
 
 int main (int argc, char *argv[])
 {
-    int t, n, i, skip;
+    int t, n, i, skip, v;
 
     scanf ("%d", &t);
 
     while (t--) {
+        clear ();
         scanf ("%d", &n);
 
-        for (i = 0; i < n; i++)
-            scanf ("%d", &weight[i]);
-        for (i = 0; i < n; i++)
-            scanf ("%d", &limit[i]);
+        for (i = 0; i < n; i++) {
+            scanf ("%d", &v);
+            weight[i] = v;
+        }
+        for (i = 0; i < n; i++) {
+            scanf ("%d", &v);
+            limit[i] = v;
+        }
 
-        qsort (weight, n, sizeof (int), comp);
-        qsort (limit, n, sizeof (int), comp);
+        qsort (weight, n, sizeof (weight[0]), comp);
+        qsort (limit, n, sizeof (limit[0]), comp);
         max = n;
 
-        printf ("%d\n", solve (0, 0));
+        printf ("%d\n", solve ());
     }
 
     return 0;

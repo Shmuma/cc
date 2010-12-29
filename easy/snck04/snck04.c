26fdd5deb02dede3a11198308a06bbcbce632203
Max Lapan <max.lapan@gmail.com>
2010-12-29 17:51:50 +0300

    snck04, cont

--- easy/snck04/snck04.c
+++ easy/snck04/snck04.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 
+int solve (int n);
+
+
+
 int main (int argc, char *argv[])
 {
     int t, n;
@@ -12,4 +16,40 @@ int main (int argc, char *argv[])
         printf ("%d\n", solve (n));
     }
     return 0;
+}
+
+
+inline int abs (int n)
+{
+    return n < 0 ? -n : n;
+}
+
+
+int gcd (int a, int b)
+{
+    printf ("gcd %d, %d\n", a, b);
+    while (b)
+        b ^= a ^= b ^= a %= b;
+    return a;
+}
+
+
+/* Need to check this: http://en.wikipedia.org/wiki/Coprime#Generating_all_coprime_pairs */
+
+int solve (int n)
+{
+    int i, j, res = 0;
+    int j_inc;
+
+    for (i = 1; i <= n; i++) {
+        j = i+1;
+        j_inc = 2 - (i % 2);
+        while (j <= n) {
+            if (gcd (i, j) == 1)
+                res++;
+            j += j_inc;
+        }
+    }
+
+    return res;
 }

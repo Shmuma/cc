88d15032ebd9252d6d7bed1c584605fa42430515
Max Lapan <max.lapan@gmail.com>
2010-12-30 01:10:06 +0300

    Dce04 accepted

--- easy/dce04/dce04.c
+++ easy/dce04/dce04.c
@@ -17,82 +17,6 @@ typedef struct {
 } src_t;
 
 
-int add_c (src_t* src, dest_t* dest);
-int add_w (src_t* src, dest_t* dest);
-int add_m (src_t* src, dest_t* dest);
-
-
-int finish (src_t* src)
-{
-    /* ther are no place on ship */
-    if (!src->total)
-        return 1;
-    /* we placed all people */
-    if (!src->c && !src->w && !src->m)
-        return 1;
-    return 0;
-}
-
-
-/* add one children */
-int add_c (src_t* src, dest_t* dest)
-{
-    if (!src->c || !src->total)
-        return 0;
-    src->c--;
-    src->total--;
-    dest->c++;
-
-    if (((dest->c + 3) >> 2) > (dest->w + dest->m)) {
-        /* we need an adult to be placed */
-        if (!add_w (src, dest))
-            if (!add_m (src, dest)) {
-                /* rollback */
-                src->c++;
-                src->total++;
-                dest->c--;
-                return 0;
-            }
-    }
-
-    return 1;
-}
-
-
-/* add an woman */
-int add_w (src_t* src, dest_t* dest)
-{
-    if (!src->w || !src->total)
-        return 0;
-
-    src->w--;
-    src->total--;
-    dest->w++;
-
-    if (((dest->w+1) >> 1) > dest->m) {
-        if (!add_m (src, dest)) {
-            src->w++;
-            src->total++;
-            dest->w--;
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
-
-int add_m (src_t* src, dest_t* dest)
-{
-    if (!src->m || !src->total)
-        return 0;
-
-    src->m--;
-    src->total--;
-    dest->m++;
-    return 1;
-}
-
 /*
   constraints:
   1. res_c + res_w + res_m <= cap
@@ -104,13 +28,19 @@ int add_m (src_t* src, dest_t* dest)
  */
 void solve (src_t* src, dest_t* dest)
 {
+    int i;
+
     dest->c = dest->w = dest->m = 0;
 
-    while (1) {
-        if (!add_c (src, dest))
-            if (!add_w (src, dest))
-                if (!add_m (src, dest))
-                    break;
+    for (i = 0; i < src->total; i++) {
+        if (dest->c < 4*(dest->w + dest->m) && dest->c < src->c)
+            dest->c++;
+        else
+            if (dest->w < 2*dest->m && dest->w < src->w)
+                dest->w++;
+            else
+                if (dest->m < src->m)
+                    dest->m++;
     }
 }
 

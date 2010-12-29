6f79389e1f480963fc4913f43daad9a94617aaae
Max Lapan <max.lapan@gmail.com>
2010-12-29 15:36:16 +0300

    A bit fster

--- easy/Equation/equation.c
+++ easy/Equation/equation.c
@@ -2,25 +2,14 @@
 #include <stdlib.h>
 
 
-unsigned long long solve2 (int n, int c)
-{
-    printf ("solve2 (%d, %d)\n", n, c);
-
-    if (n > c)
-        return c+1;
-    else
-        return n+1;
-}
-
-
 #define MIN(a, b) ((a < b) ? (a) : (b))
 
 
-unsigned long long solve3 (int n, int b, int c)
+unsigned int solve3 (int n, int b, int c)
 {
     int i;
 
-    unsigned long long res = 0;
+    unsigned int res = 0;
 
     //    printf ("solve3 (%d, %d, %d)\n", n, b, c);
 
@@ -32,10 +21,10 @@ unsigned long long solve3 (int n, int b, int c)
 }
 
 
-unsigned long long solve4 (int n, int a, int b, int c)
+unsigned int solve4 (int n, int a, int b, int c)
 {
     int i;
-    unsigned long long res = 0;
+    unsigned int res = 0;
 
     //    printf ("solve4 (%d, %d, %d, %d)\n", n, a, b, c);
 
@@ -54,7 +43,7 @@ int main (int argc, char *argv[])
 
     while (t--) {
         scanf ("%d %d %d %d", &n, &a, &b, &c);
-        printf ("%llu\n", solve4 (n, a, b, c));
+        printf ("%u\n", solve4 (n, a, b, c));
     }
 
     return 0;

796c20889c158b2132f83ea15639f96c311d8ac5
Max Lapan <max.lapan@gmail.com>
2012-07-14 20:36:58 +0400

    Planner outline

--- others/icfp-2012/lib/planner.py
+++ others/icfp-2012/lib/planner.py
@@ -1,6 +1,6 @@
-def build (world, goal):
+def make_plan (world, goal):
     """
     Builds a plan to reach a goal point with best score. Returns pair with plan string and score obtained.
     """
-    
+    return "A", 0
 
--- others/icfp-2012/mining.py
+++ others/icfp-2012/mining.py
@@ -4,51 +4,20 @@ import sys
 import logging
 
 from lib.world import World
-
-
-def show_map (map_data):
-    print "Robot: %d, %d" % map_data['robot']
-
-    for i in range (len (map_data['map'])):
-        print map_data['map'][-(i+1)]
-    print ""
-
-
-def move_robot (map_data, dx, dy):
-    """
-    Create new map according robot movement
-    """
-    x, y = map_data['robot']
-    nx, ny = x + dx, y + dy
-
-
-def next_map (map_data, action):
-    """
-    Calculates next map according to action applied. Action should be one of:
-    - LRUD - move robot
-    - W - wait
-    """
-    if action in "LRUD":
-        # check action for validity
-        dx, dy = {"L": (-1, 0), "R": (+1, 0), "U": (0, +1), "D": (0, -1)}[action]
-        new_map_data = move_robot (map_data, dx, dy)
-        if new_map_data == None:
-            # move is invalid, ignore move request
-            logging.info ("Action '%s' is invalid, ignore it" % action)
-        else:
-            map_data = new_map_data
-    return map_data
-
-
-# read map specification
-#map_data = read_map ()
-#show_map (map_data)
+from lib.planner import make_plan
 
 
 w = World (sys.stdin.readlines ())
 w.show_full ()
 
-for a in "RRUDRRULURULLLLDDDL":
+goal = w.lambdas[0]
+print "Trying to reach lambda %s" % str (goal)
+
+plan, score = make_plan (w, goal)
+
+print "Got plan '%s' with score %d, verify" % (plan, score)
+
+for a in plan:
     print "Action '%s'" % a
     act_res = w.do_action (a)
     if act_res == None:

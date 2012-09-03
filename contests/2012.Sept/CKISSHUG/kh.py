b0b510a055be21132d6dd9e68575807fd5c2c4ea
Max Lapan <max.lapan@gmail.com>
2012-09-03 12:31:49 +0400

    accepted

--- contests/2012.Sept/CKISSHUG/kh.py
+++ contests/2012.Sept/CKISSHUG/kh.py
@@ -30,19 +30,47 @@ def solve (n):
     res = 1
     for k in range (n):
         res += 2 ** int ((n - k) / 2)
-#        res %= 1000000007
+        res %= 1000000007
     return res
+
+
+def solve1 (n):
+    p = int ((n-1)/2)
+    v = (2**p) % 1000000007
+    res = ((v-1) << 1)
+    if n % 2 == 1:
+        res += (v << 1)
+    else:
+        res += (v << 2)
+    return res % 1000000007
 
 
 def solve2 (n):   
     p = int ((n-1)/2)
     v = two_pow_mod (p)
-    res = ((v-1) << 1) % 1000000007
+    res = ((v-1) << 1)
     if n % 2 == 1:
-        res += (v << 1) % 1000000007
+        res += (v << 1)
     else:
-        res += (v << 2) % 1000000007
-    return res
+        res += (v << 2)
+    return res % 1000000007
+
+
+# mismatch = match = 0
+
+# for i in range (1, 1000000):
+#     v1 = solve2 (i)
+#     v2 = solve2 (i)
+#     if i % 1000 == 0:
+#         print "%d: Matched %d, mismatched %d" % (i, match, mismatch)
+#     if v1 != v2:
+#         mismatch += 1
+#     else:
+#         match += 1
+
+# print "Matched %d, mismatched %d" % (match, mismatch)
+
+# sys.exit (1)
 
 
 t = int (sys.stdin.readline ().strip ())
